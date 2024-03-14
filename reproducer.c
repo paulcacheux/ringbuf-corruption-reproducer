4e2cbfdd7ebb33fb22abc5e86a89658e16a5f7c1
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-03-14 13:23:26 +0100

    payload refactor and progress bar

--- reproducer.c
+++ reproducer.c
@@ -4,7 +4,7 @@
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
-#define PAYLOAD_SIZE 64
+#define PAYLOAD_SIZE (512 - 2 * 8)
 
 struct event {
 	u8 payload[PAYLOAD_SIZE];
@@ -20,10 +20,10 @@ const struct event *unused __attribute__((unused));
 
 SEC("xdp")
 int ringbuf_filler(struct pt_regs *ctx) {
-	u32 size = bpf_get_prandom_u32() % PAYLOAD_SIZE;
-
 	struct event evt = {0};
-	__builtin_memset(&evt, size, PAYLOAD_SIZE);
-	bpf_ringbuf_output(&events, &evt, size, 0);
+	u32 value = bpf_get_prandom_u32();
+	__builtin_memset(&evt, value, PAYLOAD_SIZE);
+
+	bpf_ringbuf_output(&events, &evt, PAYLOAD_SIZE, 0);
 	return 0;
 }
