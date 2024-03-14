e2d92f82a3919344603d4b9b4c6cef6a51acface
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-03-14 11:19:05 +0100

    kprobe poc from examples

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/paulcacheux/ringbuf-corruption-reproducer
+
+go 1.22.1
+
+require (
+	github.com/cilium/ebpf v0.13.2
+	golang.org/x/sys v0.18.0
+)
+
+require golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2 // indirect
---
+++ reproducer.c
@@ -0,0 +1,24 @@
+//go:build ignore
+
+#include "common.h"
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+struct event {
+	u8 payload[8];
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_RINGBUF);
+	__uint(max_entries, 1 << 12);
+} events SEC(".maps");
+
+// Force emitting struct event into the ELF.
+const struct event *unused __attribute__((unused));
+
+SEC("kprobe/sys_execve")
+int kprobe_execve(struct pt_regs *ctx) {
+	struct event evt = {0};
+	bpf_ringbuf_output(&events, &evt, sizeof(struct event), 0);
+	return 0;
+}
